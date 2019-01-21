72dbd7f71bcfa224c504adf2e8f641b0b1b203a9
EsaikaniL <46399336+EsaikaniL@users.noreply.github.com>
2019-01-21 17:03:50 +0530

    Update and rename specl.py to swap.py

--- specl.py
+++
@@ -1,7 +0,0 @@
-//esai
-a=str(input())
-b=0
-for i in range(len(a)):
-    if ("," in a)or("." in a)or(";" in a)or(":" in a)or("<" in a)or(">" in a)or("/" in a)or("?" in a)or("'" in a)or("["in a)or("]"in a)or("{" in a)or("}" in a)or("~" in a)or("!" in a)or("@" in a)or("#" in a)or("$" in a)or("%"in a)or("^" in a)or("&" in a)or("*" in a)or("(" in a)or(")" in a)or("-" in a)or("+"in a)or("="in a):
-            b=b+1
-            print(b)
---
+++ swap.py
@@ -0,0 +1,7 @@
+//esai
+a=int(input())
+b=int(input())
+c=a
+a=b
+b=c
+print(a,c)
