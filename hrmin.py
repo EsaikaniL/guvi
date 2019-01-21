9930a144c02e5e068a699542dedf2ed49611e316
EsaikaniL <46399336+EsaikaniL@users.noreply.github.com>
2019-01-21 16:40:19 +0530

    Update and rename hrmin.py to hrsmin.py

--- hrmin.py
+++
@@ -1,5 +0,0 @@
-//esai
-minute=int(input())
-hour=minute/60
-minu=minute%60
-print(hour,minute)
---
+++ hrsmin.py
@@ -0,0 +1,9 @@
+//esai
+a=int(input())
+b=int(input())
+c=b-a
+if c<0:
+    e=(c*(-1))
+    print(e)
+else:
+    print(c)
