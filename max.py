28d237dfee7ff9c8b2bf013c7050c36abc068c2e
EsaikaniL <46399336+EsaikaniL@users.noreply.github.com>
2019-01-21 15:55:01 +0530

    Update and rename max.py to min.py

--- max.py
+++
@@ -1,8 +0,0 @@
-//esaikani
-a=[]
-n=int(input())
-for i in range(1,n+1):
-    b=int(input())
-    a.append(b)
-a.sort()
-print(a[n-1])
---
+++ min.py
@@ -0,0 +1,7 @@
+//esaikani
+list1 = [] 
+num = int(input()) 
+for i in range(1, num + 1): 
+    ele = int(input()) 
+    list1.append(ele) 
+print( min(list1)) 
