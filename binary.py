eccb6ff12486e971a72cd51881a78b4f0a133b2f
EsaikaniL <46399336+EsaikaniL@users.noreply.github.com>
2019-01-22 16:55:59 +0530

    Update and rename binary.py to min2.py

--- binary.py
+++
@@ -1,7 +0,0 @@
-//esai
-a=input()
-b=list(a)
-if (b is "1")or(b is "0"):
-	print("yes")
-else:
-	print("no")
---
+++ min2.py
@@ -0,0 +1,6 @@
+//esai
+lst = []
+num = int(input())
+for n in range(num):
+    lst.append(n)
+print(min(lst))
