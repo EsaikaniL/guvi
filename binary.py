dbe988e3c0877466df3e05dde7a884985a20df20
EsaikaniL <46399336+EsaikaniL@users.noreply.github.com>
2019-01-22 16:51:28 +0530

    Update and rename frst.py to binary.py

---
+++ binary.py
@@ -0,0 +1,7 @@
+//esai
+a=input()
+b=list(a)
+if (b is "1")or(b is "0"):
+	print("yes")
+else:
+	print("no")
--- frst.py
+++
@@ -1,8 +0,0 @@
-//esai
-a=input()
-b=int(input())
-c=list(a)
-d=0
-while d<b:
-	print(c[d])
-	d=d+1
