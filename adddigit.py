8b82ec277cf23e406f13656fb08f909de9cb0155
EsaikaniL <46399336+EsaikaniL@users.noreply.github.com>
2019-01-22 16:22:18 +0530

    Update and rename adddigit.py to lesequ.py

--- adddigit.py
+++
@@ -1,9 +0,0 @@
-//esai
-num = int(input())
-result = 0
-hold = num
-while num > 0:
-    rem = num % 10
-    result = result + rem
-    num = int(num/10)
-print(result)
---
+++ lesequ.py
@@ -0,0 +1,7 @@
+//esai
+num=int(input())
+if(num%2==0):
+	ans=num-2
+else:
+	ans=num-1
+print(ans)
