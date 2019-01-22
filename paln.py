3b7582ebd26b5571416a46c815b04ca25439bc1c
EsaikaniL <46399336+EsaikaniL@users.noreply.github.com>
2019-01-22 17:21:31 +0530

    Update and rename paln.py to vowel2.py

--- paln.py
+++
@@ -1,8 +0,0 @@
-//esai
-my_tr1 = input()
-my_str = my_str.casefold()
-rev_str = reversed(my_str)
-if list(my_str) == list(rev_str):
-   print("yes")
-else:
-   print("no")
---
+++ vowel2.py
@@ -0,0 +1,7 @@
+//esai
+a=input()
+b=list(a)
+if("a" in b)or("e" in b)or("i" in b)or("o" in b)or("u" in b):
+    print("yes")
+else:
+    print("no")
